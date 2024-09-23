67cf3b10db9198148419b1ebc977e01ceabd18b9
Oka Sutarto Putra <oka.sutarto@jublia.com>
2024-09-24 00:15:22 +0700

    separate assistant controller

--- be-summarizer/controller/assistant.js
+++ be-summarizer/controller/assistant.js
@@ -7,52 +7,30 @@ const openai = new OpenAI({
 
 
 class assistantController {
-  static async assistant(req, res) {
-    let { content } = req.body
-    content =`"${content}"`
-    
-    const assistant  = await openai.beta.assistants.create({
-      model: "gpt-4o-mini",
-      name: 'Article Summarizer',
-      // instructions: `You are a article summarizer machine. Craft a summary for ${content} that is detailed, thorough, in-depth, and complex, while maintaining clarity and conciseness. If the ${content} not related to article, Just describe what is ${content}`,
-      instructions: `You are a article summarizer machine. Summarize the context of ${content}  that is detailed, thorough, in-depth, and complex, while maintaining clarity and conciseness.If the ${content} is a question, Just describe the question without giving the result`,
+  static async createAssistant (req, res) {
+    let {
+      model,
+      name,
+      instructions
+    } = req.body
+    const newAssistant  = await openai.beta.assistants.create({
+      model,
+      name,
+      instructions,
       tools: [
         {"type": "code_interpreter"}
         ],
     })
 
-    const thread = await openai.beta.threads.create()
+    res.status(200).json(newAssistant)
+  }
 
-    const message = await openai.beta.threads.messages.create(
-      thread.id,
-      {
-        role: "user",
-        content
-      }
-    );
+  static async deleteAssistant(req, res) {
+    let { id } = req.body
+    
+    const response = await openai.beta.assistants.del(id);
 
-    let run = await openai.beta.threads.runs.createAndPoll(
-      thread.id,
-      { 
-        assistant_id: assistant.id,
-        instructions: `You are a article summarizer machine. Summarize the context of ${content}  that is detailed, thorough, in-depth, and complex, while maintaining clarity and conciseness.If the ${content} is a question, Just describe the question without giving the result`
-      }
-    );
-
-    const messages = await openai.beta.threads.messages.list(
-      run.thread_id
-    );
-    // if (run.status === 'completed') {
-    // for (const message of messages.data.reverse()) {
-    //   console.log(`${message.content[0].text.value}`);
-    // }
-    // } else {
-    //   console.log(run.status);
-    // }
-
-    res.status(200).json({
-      data: messages.data[0].content[0].text.value
-    })
+    res.status(201).json(response)
   }
 }
 
