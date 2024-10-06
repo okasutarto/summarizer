fccae5285e6a72544da92af654f05cb268d91687
Oka Sutarto Putra <oka.sutarto@jublia.com>
2024-10-06 22:34:22 +0700

    fix app.listen

--- be-summarizer/app.js
+++ be-summarizer/app.js
@@ -1,7 +1,6 @@
 global.express = require('express')
 const app = express()
 require("dotenv").config()
-const port = process.eventNames.PORT || 4000
 const router = require("./routes/index")
 const cors = require('cors')
 
@@ -11,6 +10,6 @@ app
   .use(express.urlencoded({extended: false}))
   .use("/", router)
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(() => {
+  console.log("server is running")
 })
